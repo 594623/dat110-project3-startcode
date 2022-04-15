9bbfc1a0d2abe4ac27974d1d7588f8c9987c3704
krist <krist@VALHALLA.lan>
2022-04-15 18:11:24 +0200

    Finished Task 2

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/FileManager.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/FileManager.java
@@ -57,12 +57,13 @@ public class FileManager {
 		// implement
 		
 		// set a loop where size = numReplicas
-		
-		// replicate by adding the index to filename
-		
-		// hash the replica
-		
-		// store the hash in the replicafiles array.
+		for (int i = 0; i < Util.numReplicas; i++) {
+			// replicate by adding the index to filename
+			String repl = filename + i;
+			// hash the replica
+			// store the hash in the replicafiles array.
+			replicafiles[i] = Hash.hashOf(repl);
+		}
 
 	}
 	
